20512039a789709b5b19dc3790366dfae1ee3df8
Jordi Montes <jomsdev@gmail.com>
2019-05-05 15:21:33 +0200

    kmv implementation using adaptive table

---
+++ kmv.go
@@ -0,0 +1,67 @@
+package gokmv
+
+import (
+	"math"
+	"math/rand"
+	"time"
+
+	adaptivetable "github.com/positiveblue/adaptive-table"
+	murmur3 "github.com/spaolacci/murmur3"
+)
+
+type KMV struct {
+	table        adaptivetable.AdaptiveTable
+	initialSize  int
+	seed         uint32
+	totalCounter uint64
+}
+
+func NewKMV(size int) *KMV {
+	rand.Seed(time.Now().UnixNano())
+	return NewKMVWithSeed(size, rand.Uint32())
+}
+
+func NewKMVWithSeed(size int, seed uint32) *KMV {
+	return &KMV{
+		table:        adaptivetable.NewAdaptiveTableComplete(size, math.MaxInt64, size),
+		initialSize:  size,
+		seed:         seed,
+		totalCounter: 0,
+	}
+}
+
+func (kmv *KMV) ElementsAdded() uint64 {
+	return kmv.totalCounter
+}
+
+func (kmv *KMV) Size() int {
+	return kmv.table.Size()
+}
+
+func (kmv *KMV) InsertUint64(hash uint64) {
+	kmv.totalCounter++
+	kmv.table.Insert(hash)
+}
+
+func (kmv *KMV) InsertString(s string) {
+	hash := murmur3.Sum64WithSeed([]byte(s), kmv.seed)
+	kmv.InsertUint64(hash)
+}
+
+func (kmv *KMV) EstimateCardinality() uint64 {
+	if kmv.Size() < kmv.initialSize {
+		return uint64(kmv.table.Size())
+	}
+	a := uint64(kmv.table.Size())
+	b := kmv.table.Max()
+
+	c := b / a
+
+	d := uint64(math.MaxUint64 / c)
+
+	return d
+	/*
+		meanDistance := kmv.table.Max() / uint64(kmv.table.Size())
+		return uint64(math.MaxUint64 / float64(meanDistance))
+	*/
+}
