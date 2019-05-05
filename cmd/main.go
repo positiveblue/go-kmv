e9ff236566c53e7c6f3f3e97b6213ed8546059db
Jordi Montes <jomsdev@gmail.com>
2019-05-05 15:22:18 +0200

    cmd tool to estimate the cardinality from StdIn

---
+++ cmd/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	gokmv "github.com/positiveblue/go-kmv"
+)
+
+func getScanner(fileName string) *bufio.Scanner {
+	if fileName != "" {
+		f, err := os.Open(fileName)
+		if err != nil {
+			panic(err)
+		}
+		return bufio.NewScanner(f)
+	} else {
+		return bufio.NewScanner(os.Stdin)
+	}
+}
+
+func fmtMessage(estimator *gokmv.KMV) string {
+	distinct := estimator.EstimateCardinality()
+	total := estimator.ElementsAdded()
+	return fmt.Sprintf("%d distinct elements out of %d processed elements", distinct, total)
+}
+
+func main() {
+	sizePtr := flag.Int("size", 64, "initial size for the kmv data structure")
+	fileNamePtr := flag.String("filename", "", "File name to process (otherwhise will read from StdIn")
+
+	flag.Parse()
+
+	scanner := getScanner(*fileNamePtr)
+	kmv := gokmv.NewKMV(*sizePtr)
+	for scanner.Scan() {
+		for _, word := range strings.Fields(scanner.Text()) {
+			kmv.InsertString(word)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	} else {
+		fmt.Println(fmtMessage(kmv))
+	}
+}
